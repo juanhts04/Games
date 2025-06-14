e08695ff3a2f0f1afbcaf7cd48dbda9acecde2ab
juanhts04 <juancarlossintor@gmail.com>
2025-06-14 16:27:44 -0500

    user services

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../usuarios/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'https://apigame.gonzaloandreslucio.com/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+  }
+
+  // Obtener todos los usuarios
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  // Obtener un usuario por ID
+  getUsuario(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiUrl}/${id}`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  /**
+   * Crea un nuevo usuario.
+   * El backend debe encargarse de asignar el ID.
+   */
+  crearUsuario(usuario: Omit<Usuario, 'id'>): Observable<Usuario> {
+    return this.http.post<Usuario>(this.apiUrl, usuario, {
+      headers: this.getHeaders(),
+    });
+  }
+}
